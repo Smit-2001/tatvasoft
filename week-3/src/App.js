c305d90c96e8b5841996dfafe07c0394e2bbba44
Smit <smitpshah63@gmail.com>
2023-03-22 21:48:53 +0530

    third week commit

---
+++ week-3/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import SignInOutContainer from './containers';
+import abc from './components/abc'
+function App() {
+  return (
+    <div className="App">
+     <SignInOutContainer/>
+     {/* <abc/>  */}
+    </div>
+  );
+} 
+
+export default App;
